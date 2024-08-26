96be19eca486c958aa461793bbd87d4e01bd8609
hawetan <hawetanzerihun@gmail.com>
2024-08-26 05:51:37 -0700

    calculator.py
    Co-Authored-By: Yared Kebede <135129440+Yareda21@users.noreply.github.com>

--- calculator.py
+++ calculator.py
@@ -1,1 +1,13 @@
+while True:
+    num1=(int(input("num1: - ")))
+    num2=(int(input("num2: - ")))
+    operator=input("operator: - ")
+    if operator=="+":
+        print(num1+num2)
+    elif "-":
+        print(num1-num2) 
+    elif "*":
+        print(num1*num2) 
+    elif "/":
+        print(num1/num2)          
 
